c89aa4791b6518bf64f69bd16db0f04d7f6069a3
krane41 <keran.le.foll@gmail.com>
2019-08-22 16:35:09 +0200

    Add files via upload

---
+++ S1_SNT.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # SNT
+# 
+# ## Séance_1
+# 
+# ### Langage : introduction à Python
+
+# 1. Mon premier programme :
+# 
+# - une entrée : ton prénom
+# - une sortie : un message de bienvenue personnalisé
+
+# In[ ]:
+
+
+a=input("Quel est ton prénom ?")
+print("Bienvenue,",a,",au cours de SNT.")
+
+
+# Exercice 1 : Écris un programme qui demande ton prénom puis ton nom de famille et qui te souhaite la bienvenue au cours de SNT.
+
+# In[ ]:
+
+
+# Exercice 1 : 
+
+
+
+# In[1]:
+
+
+from turtle import*
+
+forward(100)# avance de 100 pixels
+
+exitonclick() # permet de fermer la fenêtre 
+
+
+# In[ ]:
+
+
+
+
